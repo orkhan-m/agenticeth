a327fe48bd1bc8f30a8974469476a37b77744684
orkhan-m <orkhanmirzayev96@gmail.com>
2025-02-07 01:30:08 +0100

    connect openai and create input field for generation

--- client/src/pages/GenerateCollection.jsx
+++ client/src/pages/GenerateCollection.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/NavBar";
 import ButtonBar from "../components/ButtonBar";
+import OpenAiItem from "../components/OpenAiItem";
 
 export default function GenerateCollection() {
   return (
     <>
       <NavBar />
       <ButtonBar />
+      <OpenAiItem />
     </>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
+// dotenv
+import dotenv from "dotenv";
+
+dotenv.config(); // load env vars from .env
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  define: {
+    __VALUE__: `"${process.env.VITE_OPENAI_API_KEY}"`, // eslint-disable-line no-undef
+  },
 });
